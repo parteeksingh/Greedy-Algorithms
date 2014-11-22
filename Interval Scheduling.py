4d17c650c1e49b36f61dcec0c67289293a5ca185
Parteek Singh <parteek16@gmail.com>
2014-11-22 18:26:00 +0530

    Interval Scheduling Algorithm using greedy approach

---
+++ Interval Scheduling.py
@@ -0,0 +1,36 @@
+data = []
+schedule =  []
+def readFile(fname):
+    fid = open(fname)    
+    for line in fid:
+        temp =[x.rstrip("\n") for x in line.split(" ")]
+        data.append((temp[0],int(temp[1]),int(temp[2])))
+  
+def getKey(item):
+    return item[2]
+
+def  checkForCompatibility(job):
+    if len(schedule) == 0:
+        return True
+    if job[1] < schedule[-1][2]:
+        return False
+    else:
+        return True
+
+
+readFile("Input.txt")
+data = sorted(data, key=getKey)
+for job in data:
+    if checkForCompatibility(job):
+        schedule.append(job)
+        
+    
+
+
+print("The Optimal Job scheduling is :")
+for eachJob in schedule:
+    print(eachJob)
+    
+
+
+   
